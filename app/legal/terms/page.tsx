f7cbd311da013e1f760b50d938cbb6b5501f3d89
Srijan Baniyal <john.rambo.9901@gmail.com>
2025-06-25 17:49:19 +0530

    feat: add careers, privacy, and terms pages with layouts; update footer links

---
+++ app/legal/terms/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function Terms() {
+  return (
+    <>
+      <Navbar />
+      <div>Terms</div>
+      <Footer />
+    </>
+  );
+}
---
+++ app/newsroom/layout.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Newsroom | Stay Updated",
+  description:
+    "Stay updated with the latest news and announcements from Insturix. Get the latest insights, updates, and announcements about our products, services, and more.",
+  keywords:
+    "Newsroom Insturix , Stay Updated , Insturix news , News updates , Announcements at Insturix , Latest news , Updates at Insturix",
+  openGraph: {
+    title: "Newsroom | Stay Updated",
+    description:
+      "Stay updated with the latest news and announcements from Insturix. Get the latest insights, updates, and announcements about our products, services, and more.",
+    images: [
+      {
+        url: "/icons/contact-og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Newsroom Insturix - Stay Updated",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Newsroom | Stay Updated",
+    description:
+    "Stay updated with the latest news and announcements from Insturix. Get the latest insights, updates, and announcements about our products, services, and more.",
+    images: ["/icons/newsroom-twitter-image.jpg"],
+  },
+};
+
+export default function NewsroomLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
